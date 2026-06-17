8719c82af5850346d0cd0049ef7a7c0f8cc022d8
noel-odero <anoel.odero@gmail.com>
2026-06-17 19:24:07 +0200

    fix: change delete behavior for Product foreign key to Restrict in Assets table

---
+++ ProductCatalogue/Migrations/20260617172320_ChangeToRestrict.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalogue.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeToRestrict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_Products_ProductId",
+                table: "Assets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_Products_ProductId",
+                table: "Assets",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_Products_ProductId",
+                table: "Assets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_Products_ProductId",
+                table: "Assets",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
