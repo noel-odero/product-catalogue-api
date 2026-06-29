4324f0be4ea97fbfeab6f7877e629468734eba85
noel-odero <anoel.odero@gmail.com>
2026-06-29 14:52:44 +0200

    feat: add event envelope and payloads for asset and product events

---
+++ ProductCatalogue/Events/EventEnvelope.cs
@@ -0,0 +1,10 @@
+namespace ProductCatalogue.Events;
+
+public class EventEnvelope<TPayload>
+{
+    public Guid EventId { get; init; } = Guid.NewGuid();
+    public string EventType { get; init; } = string.Empty;
+    public DateTimeOffset OccurredAt { get; init; } = DateTimeOffset.UtcNow;
+    public int Version { get; init; } = 1;
+    public TPayload Payload { get; init; } = default!;
+}
---
+++ ProductCatalogue/Events/EventTypes.cs
@@ -0,0 +1,10 @@
+namespace ProductCatalogue.Events;
+
+public static class EventTypes
+{
+    public const string AssetUploaded = "AssetUploaded";
+    public const string AssetApproved = "AssetApproved";
+    public const string AssetRejected = "AssetRejected";
+    public const string ProductSubmittedForReview = "ProductSubmittedForReview";
+    public const string ProductPublished = "ProductPublished";
+}
